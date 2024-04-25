2c6209a47f8fe7e55362cd28e0d89f5c5b9e2778
Vad_Siam <nevod80@gmail.com>
2024-04-25 21:34:43 +0700

    added counter and recommended products

--- [lng]/components/ThemesComponents/StyledLink.tsx
+++ [lng]/components/ThemesComponents/StyledLink.tsx
@@ -10,13 +10,13 @@ const StyledLink = (props: StyledLinkProps) => {
   const { text, href, textButton } = props;
 
   const linkStyleClasses = textButton
-    ? "bg-transparent hover:bg-btn-text-background-hover dark:hover:bg-dark-btn-background-hover text-btn-text dark:text-dark-btn-text"
-    : "py-2 px-3 flex rounded-md no-underline bg-btn-background text-foreground hover:bg-btn-background-hover dark:bg-background-white dark:text-foreground-dark-gray"
-
+  ? "bg-transparent hover:bg-btn-text-background-hover dark:hover:bg-dark-btn-background-hover text-btn-text dark:text-dark-btn-text"
+  : "py-2 px-3 flex items- rounded-md no-underline bg-btn-background text-foreground hover:bg-btn-background-hover dark:bg-background-white dark:text-foreground-dark-gray"
+  
   return (
     <Link
       href={href}
-      className={`py-2 px-3 flex rounded-md no-underline ${linkStyleClasses}`}
+      className={`py-2 px-3 flex rounded-md no-underline items-center ${linkStyleClasses}`}
     >
       {text}
     </Link>
--- app/[lng]/catalog/[id]/Specs.tsx
+++ app/[lng]/catalog/[id]/Specs.tsx
@@ -27,7 +27,7 @@ const Specs: FC<ISpec> = ({
         <button onClick={showDetails} type="button" className="flex items-center justify-between w-full p-5 pl-0 font-medium rtl:text-right text-gray-500 dark:text-gray-400 gap-3" data-accordion-target="#accordion-collapse-body-1" aria-expanded="true" aria-controls="accordion-collapse-body-1">
           <span>{t('Спецификация')}</span>
           <svg data-accordion-icon className={`w-3 h-3 ${show ? 'rotate-0' : 'rotate-180'} shrink-0`} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5 5 1 1 5" />
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5 5 1 1 5" />
           </svg>
         </button>
       </h2>
